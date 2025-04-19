512d8b521b7170cfc951c909359c166824b51277
zhugechina <zhugetd@126.com>
2025-04-20 01:31:01 +0800

    add adsterra

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,10 +1,8 @@
 /* eslint-env node */
 
-import { siteUrl } from "./config";
-
 /** @type {import('next-sitemap').IConfig} */
 export default {
-  siteUrl: siteUrl,
+  siteUrl: "https://crazycattle3d.fun",
   generateIndexSitemap: false,
   generateRobotsTxt: false,
   // output: "export", // Set static output here
